0c8eb337815251ddca6d0080fc914c470cdf2a31
Nastya Gordeeva <nastyagordeeva72@gmail.com>
2024-04-09 03:34:19 +0300

    M-Created intro,login,and half main activity

---
+++ app/src/main/java/com/example/movieapplication/IntroActivity.java
@@ -0,0 +1,24 @@
+package com.example.movieapplication;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button getInBtn = findViewById(R.id.getInBtn);
+        getInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/movieapplication/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.movieapplication;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userEdt,passEdt;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        userEdt=findViewById(R.id.editTextText);
+        passEdt=findViewById(R.id.editTextPassword);
+        loginBtn=findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()) {
+                            Toast.makeText(LoginActivity.this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
+                        } else if (userEdt.getText().toString().equals("test") && passEdt.getText().toString().equals("test")) {
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "You entered wrong username and password", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+        );
+    }
+}
